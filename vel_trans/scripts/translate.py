705f9b1c330c1c90bcf9f00e6b41a017d9282f55
kingMordred <80642142+kingMordred@users.noreply.github.com>
2023-03-29 02:30:17 -0700

    Add files via upload

--- vel_trans/scripts/translate.py
+++ vel_trans/scripts/translate.py
@@ -13,7 +13,26 @@ def joint_callback(data):
     twist.angular.x=data.angular.x
     twist.angular.y=data.angular.y
     twist.angular.z=data.angular.z"""
-    data.linear.x*=-1
+    #data.linear.x*=-1
+    if data.angular.z > 0 :
+        data.linear.x=(data.angular.z)*(-1) 
+        data.angular.z = 0
+    elif data.angular.z < 0 :
+        data.linear.x=(data.angular.z)*(-1)
+        data.angular.z = 0
+
+    elif data.linear.x > 0 :
+        data.angular.z=(data.linear.x)
+        data.linear.x = 0
+    elif data.linear.x < 0 :
+        data.angular.z=(data.linear.x)
+        data.linear.x = 0
+    """x = data.linear.x
+    z = data.angular.z 
+    data.linear.x= z * (-1)
+    data.angular.z = 0
+    data.angular.z= x
+    data.linear.x = 0"""
 
     joint_pub.publish(data) # Send it when ready!
 
@@ -21,7 +40,7 @@ if __name__ == '__main__':
     # Init ROS
     rospy.init_node('joint_logger_node', anonymous=True)
     # Subscribers
-    rospy.Subscriber('turtle1/cmd_vel', Twist, joint_callback)
+    rospy.Subscriber('cmd_vel', Twist, joint_callback)
     # Publishers
     joint_pub = rospy.Publisher('test_topic', Twist, queue_size = 10)
     # Spin
