5efdad27472e2780d2d0216b71f2c6f6c0353370
kingMordred <80642142+kingMordred@users.noreply.github.com>
2023-03-29 02:39:36 -0700

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -5,7 +5,7 @@ title3="arduino serial"
 
 cmd1="source devel/setup.bash;roslaunch navstack_pub spark.launch"
 cmd3="source devel/setup.bash;rosrun vel_trans translate.py"
-cmd2="sudo chmod 666 /dev/ttyACM0;rosrun rosserial_python serial_node.py _port:=/dev/ttyACM0 _baud:=115200"
+cmd2="chmod 666 /dev/ttyACM0;rosrun rosserial_python serial_node.py _port:=/dev/ttyACM0 _baud:=115200"
 
 gnome-terminal --tab --title="$title1" --command="bash -c '$cmd1; $SHELL'" \
                --tab --title="$title2" --command="bash -c '$cmd2; $SHELL'" \
